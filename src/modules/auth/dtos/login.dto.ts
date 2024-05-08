0e55010a7a86124d9208e88d9109bc2b74828de4
OscarEd08 <oscar.pairazaman2@unmsm.edu.pe>
2024-05-07 19:02:12 -0500

    Se quitó los clases para validar los propiedades del DTO de login

--- src/modules/auth/dtos/login.dto.ts
+++ src/modules/auth/dtos/login.dto.ts
@@ -1,12 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsStrongPassword } from "class-validator";
 
 export class LoginDto {
-    @ApiProperty({example: 'example@unmsm.edu.pe', description: 'The email of the user with the UNMSM domain'})
-    @IsEmail()
+    @ApiProperty({example: 'example@unmsm.edu.pe', description: 'The email of the user with the UNMSM domain'})    
     email: string;
 
-    @ApiProperty({example: 'password', description: 'The password of the user'})
-    @IsStrongPassword()
+    @ApiProperty({example: 'password', description: 'The password of the user'})    
     password: string;
 }
